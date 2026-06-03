3c6c878e249f8fa8237f75bc7085f5379d2b33c1
Aravind <aravind73205@gmail.com>
2026-06-03 17:59:28 +0530

    feat: add feature engg pipeline skeleton

---
+++ ml/feature_engineering.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+class FeatureEngineeringPipeline:
+    """
+    Feature engineering pipeline for churn prediction.
+    """
+
+    def load_dataset(self):
+        pass
+
+    def validate_dataset(self):
+        pass
+
+    def create_features(self):
+        pass
+
+    def encode_features(self):
+        pass
+
+    def split_dataset(self):
+        pass
+
+    def save_processed_dataset(self):
+        pass
+
+    def prepare_training_data(self):
+        pass
